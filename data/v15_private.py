cf9c370424136b0d24dedbece836c413d0e0efbd
Matej Roguljic <matej.roguljic@gmail.com>
2026-02-05 07:11:16 -0600

    update private signal

--- data/v15_private.py
+++ data/v15_private.py
@@ -4,7 +4,7 @@ def get_datasets():
     return {
         "2024": {
             "Signal": {
-                "Signal_20260120_1723": "/store/user/roguljic/HMDS/signal"
+                "Signal": "/store/user/roguljic/HMDS/signal"
             }
         }
     }
