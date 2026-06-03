8c9f7d395cd4d7212797af41f87a530d090d8b73
Matej Roguljić <matej.roguljic@gmail.com>
2026-06-03 16:36:15 +0200

    private signal in data

---
+++ data/v15_private.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+def get_datasets():
+    return {
+        "2024": {
+            "Signal": {
+                "Signal": "/store/user/roguljic/HMDS/signal"
+            }
+        }
+    }
